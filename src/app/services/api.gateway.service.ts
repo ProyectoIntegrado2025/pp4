55c09c88fff8a62ba8c16538f4c9d480057aeb3f
Vic <victoreduardo.cancinos@gmail.com>
2025-09-01 00:34:30 -0300

    reconfigurado para coincidir con apigateway con http api en lugar de api rest

--- src/app/services/api.gateway.service.ts
+++ src/app/services/api.gateway.service.ts
@@ -9,57 +9,90 @@ export class ApiGatewayService {
 
   constructor(private http: HttpClient) {}
 
-  private apiUrl = "https://8uv9ny98ub.execute-api.us-east-1.amazonaws.com/V1";
+  private apiUrl = "https://6w2ocnpc54.execute-api.us-east-1.amazonaws.com";
   private usersUrl = "/users"
   private groupsUrl = "/groups"
   private tasksUrl = "/tasks"
 
   // Obtiene todos los usuarios
-  getUsers():Observable<any> {
+  getUsers(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.usersUrl}`);
+  }  
+ /*  getUsers():Observable<any> {
     return this.http.get<any>(this.apiUrl+this.usersUrl);
-  }
+  } */
+
 
   // Obtiene un usuario por ID
   getUser(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.usersUrl}/${id}`);
+  }  
+/*   getUser(id: string): Observable<any> {
     const params = new HttpParams().set('id', id);
     return this.http.get<any>(this.apiUrl+this.usersUrl, { params });
-  }
+  } */
+
 
   // Obtiene todos los grupos
-  getGroups():Observable<any> {
-    return this.http.get<any>(this.apiUrl+this.groupsUrl);
+  getGroups(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.groupsUrl}`);
   }
+/*   getGroups():Observable<any> {
+    return this.http.get<any>(this.apiUrl+this.groupsUrl);
+  } */
+
 
   // Obtiene un grupo por ID
-  getGroup(id: string): Observable<any> {
+    getGroup(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.groupsUrl}/${id}`);
+  }
+  /* getGroup(id: string): Observable<any> {
     const params = new HttpParams().set('id', id);
     return this.http.get<any>(this.apiUrl+this.groupsUrl, { params });
-  }
+  } */
+
 
   // Obtiene todas las tareas
-  getTasks():Observable<any> {
-    return this.http.get<any>(this.apiUrl+this.tasksUrl);
+  getTasks(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.tasksUrl}`);
   }
+  /* getTasks():Observable<any> {
+    return this.http.get<any>(this.apiUrl+this.tasksUrl);
+  } */
+
 
   // Obtiene una tarea por ID
   getTask(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.tasksUrl}/${id}`);
+  }
+  /* getTask(id: string): Observable<any> {
     const params = new HttpParams().set('id', id);
     return this.http.get<any>(this.apiUrl+this.tasksUrl, { params });
-  }
+  } */
+
 
   // Crear tarea
-  postTask(task: any): Observable<any>{
+  postTask(task: any): Observable<any> {
+    const url = `${this.apiUrl}${this.tasksUrl}`;
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(url, JSON.stringify(task), { headers });
+  }
+  /* postTask(task: any): Observable<any>{
     const url = `${this.apiUrl}${this.tasksUrl}`;
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
     });
 
     return this.http.post(url, JSON.stringify(task), { headers });
-  }
+  } */
+
 
   // Elimnar tarea
-  deleteTask(taskId: any): Observable<any>{
+  deleteTask(taskId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}${this.tasksUrl}/${taskId}`);
+  }
+/*   deleteTask(taskId: any): Observable<any>{
     const params = new HttpParams().set('id', taskId);
     return this.http.delete(`${this.apiUrl}${this.tasksUrl}`, { params });
-  }
+  } */
 }
