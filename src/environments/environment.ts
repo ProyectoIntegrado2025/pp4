351a4460f27e70062839d389b37121c8bc63e720
Melanie_Zurdo <melaniezurdo1995@gmail.com>
2025-10-07 16:19:17 -0300

    Cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
 
     cognitoUserPoolId: 'us-east-1_Z0j1v1VS4',
     cognitoAppClientId: '55mr34sd21dhpsvaeebj3f3v63',
-    apiAssistantUrl: 'https://6odr740mod.execute-api.us-east-1.amazonaws.com/assistant'
+    apiAssistantUrl: 'https://v6w2473312.execute-api.us-east-1.amazonaws.com/dev/assistant'
 }
